43e6f16448ceb1c3705fb8be934d8daa940c1178
ntijoh-jack-blomquist <jack.blomquist@elev.ga.ntig.se>
2022-10-11 14:26:38 +0200

    styling för searchbar ps: materialize suger fan

---
+++ js/searchbarComponent.js
@@ -0,0 +1,35 @@
+const template = document.createElement('template');
+template.innerHTML = 
+`
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<nav>
+    <div class="green lighten-1">
+        <div class="input-field">
+            <i class="material-icons prefix">person_search</i>
+            <input type="text" id="inputbar" class="validate" placeholder="Enter your github username here">
+        </div>
+    </div>
+</nav>
+`;
+
+class searchbarComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode:'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // eventlistener som skriver ut vad som står i textfältet, primärt för debugging
+        this.shadowRoot.querySelector('#inputbar').addEventListener('keyup', () => {
+            console.log(this.searchbarContent);
+        });
+    }
+
+    // denna metoden skriver ut det som står i textfältet
+    get searchbarContent() {
+        return (this.shadowRoot.querySelector('input').value);
+    }
+}
+
+window.customElements.define('search-bar', searchbarComponent)
