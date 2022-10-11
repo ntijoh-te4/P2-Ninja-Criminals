2d52c6d696efa4a103ccb2e4ef728f8aad9263ca
ntijoh-jack-blomquist <jack.blomquist@elev.ga.ntig.se>
2022-10-11 14:33:08 +0200

    updaterade styling med imports

--- js/searchbarComponent.js
+++ js/searchbarComponent.js
@@ -1,8 +1,10 @@
 const template = document.createElement('template');
 template.innerHTML = 
 `
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<style>
+    @import "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css";
+    @import "https://fonts.googleapis.com/icon?family=Material+Icons";
+</style>
 
 <nav>
     <div class="green lighten-1">
