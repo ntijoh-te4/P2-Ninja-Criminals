4180f54243130f2787761ab26abfffac0d16206b
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-10-11 13:05:08 +0200

    feat: get-repo component

---
+++ js/get-repo.js
@@ -0,0 +1,21 @@
+const getRepoTemplate = document.createElement('template');
+getRepoTemplate.innerHTML = `
+    <section>
+        <h3>Repo name</h3>
+        <section>
+            <a href="">show forks</a>
+            <p>|<p>
+            <a href="">show on github</a>
+        </section>
+    </section>
+`;
+
+class GetRepoTemplate extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+        this.shadowRoot.appendChild(getRepoTemplate.content.cloneNode(true));
+    }
+}
+
+window.customElements.define('get-repo', GetRepoTemplate);
