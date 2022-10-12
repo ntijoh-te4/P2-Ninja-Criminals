30599ba16f5fb6386896b96f7b4a3b62f05a054d
ntijoh-jack-blomquist <jack.blomquist@elev.ga.ntig.se>
2022-10-12 10:58:36 +0200

    rubrik när man söker

--- js/get-repos.js
+++ js/get-repos.js
@@ -19,6 +19,10 @@ const getRepos = async (username) => {
             //add function to get forks
         }
     });
+    const repoUserTitle = document.createElement('h3');
+    repoUserTitle.textContent = `Show repos of ${res[0].owner.login}`;
+
+    main.appendChild(repoUserTitle);
 
     res.forEach(element => {
         const info = {name: element.name, forks: element.forks_url, link:element.html_url};
