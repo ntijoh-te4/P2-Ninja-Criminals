bad18c7650a072dcebdc6a2a5aa974c93e4c11bb
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-10-14 13:44:14 +0200

    flyttar get repos funktionen till index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
+const main = document.querySelector('main');
+
+const getRepos = async (username) => {
+    main.style.display = 'flex'
+    main.style.flexWrap = 'wrap'
+    main.style.rowGap = '1rem'
+    main.style.justifyContent = 'center'
+    await API.fetch(`https://api.github.com/users/${username}/repos`)
+        .then((req) => {
+            return req.json()
+        }).then((res) => {
+            res.forEach(element => {
+                const info = {name: element.name, forks: element.forks_url, link:element.html_url};
+                main.appendChild(new RepoList({info}));
+            });
+        }).catch((_error) => {
+            const errorText = document.createElement('p')
+            errorText.textContent = "...who doesn't exist..."
+            main.appendChild(errorText)
+        })
+    
+    main.addEventListener('click', async (e) => {
+        e.stopPropagation()
+        if(e.composedPath()[0].className === "show-fork"){
+            e.preventDefault()
+
+            main.innerHTML = ''
+            const resp = await API.fetch(`https://api.github.com/repos/${username}/${e.target.getAttribute('repo-name')}/contents/.manifest.json`);
+            const data = await resp.json();
+            const decoded = atob(data.content)
+            const parsedData = JSON.parse(decoded)
+            const forkList = await API.fetch(e.target.getAttribute('repo-forks-url'))
+            const forkListResp = await forkList.json()
+            forkListResp.forEach(async (item) => {
+                const forkReq = await API.fetch(item.url)
+                const forkResp = await forkReq.json()
+                main.appendChild(new ForkList(parsedData, forkResp))
+            })
+        }
+    });
+} 
 
 const getCookieValue = (name) => {
     return document.cookie.split('; ').find(row => row.startsWith(name+'='))?.split('=')[1]; 
@@ -19,7 +60,7 @@ const resetGreeting = () => {
     main.innerHTML = `
         <h1>Welcome to  Teacher-o-Matic${getCookieValue('name') ? '\n'+getCookieValue('name') : ""}!</h1>
         <p>Enter your GitHub username in the header field</p>
-    `
+    `;
 }
 
 resetGreeting();
